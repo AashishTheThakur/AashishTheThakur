58337fedb7dc01f353cf97a408a025cfe1bed32b
aashishthethakur <ashishthakur01510@gmail.com>
2021-06-09 18:17:31 +0530

    my website

--- assets/js/main.js
+++ assets/js/main.js
@@ -179,7 +179,7 @@
 
   /**
    * Initiate portfolio lightbox 
-   */
+   --------------------------------------------
   const portfolioLightbox = GLightbox({
     selector: '.portfolio-lightbox'
   });
